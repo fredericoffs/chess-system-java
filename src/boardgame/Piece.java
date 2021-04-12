71c5331024842de88b88a802439ad86370221ab9
Frederico F Silva <fredericoffs@live.com>
2021-04-12 19:06:20 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,10 +1,10 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
@@ -13,6 +13,28 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	
+
+	public abstract boolean[][] possibleMoves();
+
+	/*
+	 * rookie methods - gancho com a subclasse, chamando uma possível implementação,
+	 * template method só fará sentido quando houver uma classe concreta que impleta
+	 * o método abstrato.
+	 */
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -60,6 +60,10 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position.");
 		}
+		// se não tiver nenhum movimento possível 
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece.");
+		}
 	}
 
 	// recebe as coordenadas da peça na camada do xadrez
